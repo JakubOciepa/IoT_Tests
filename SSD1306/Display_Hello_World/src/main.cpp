8fb7e3f137bf5cce724697e3852563394136cdcb
Kuba <jakub.ociepa@outlook.com>
2021-07-18 11:23:11 +0200

    Playing around text

--- SSD1306/Display_Hello_World/src/main.cpp
+++ SSD1306/Display_Hello_World/src/main.cpp
@@ -6,14 +6,18 @@ SSD1306Wire display(0x3c, 4, 5);
 
 void displayText(void)
 {
-  display.setLogBuffer(1, 30);
+  int16_t x = 0;
+  int16_t y = 0;
+   // Demonstrates the 3 included default sizes. The fonts come from SSD1306Fonts.h file
+  // Besides the default fonts there will be a program to convert TrueType fonts into this format
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_10);
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0 + x, 10 + y, "wilogtnosc:");
 
-  display.clear();
-
-  display.println("Hello World!!");
-
-  display.drawLogBuffer(0,0);
-
+  display.setFont(ArialMT_Plain_24);
+  display.drawString(10 + x, 24 + y, "84%");
+  
   display.display();
 }
 
@@ -22,6 +26,8 @@ void setup()
   display.init();
 
   display.setContrast(255);
+
+  display.flipScreenVertically();
 
   displayText();
 }
